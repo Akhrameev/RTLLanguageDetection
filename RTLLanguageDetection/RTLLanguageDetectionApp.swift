c110bdc9a64aca9f81100c3b71e63fcbdead12b2
Pavel Akhrameev <pavel.akhrameev@picsart.com>
2022-07-20 16:41:38 +0400

    Initial Commit

---
+++ RTLLanguageDetection/RTLLanguageDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTLLanguageDetectionApp.swift
+//  RTLLanguageDetection
+//
+//  Created by Pavel Akhrameev on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RTLLanguageDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
